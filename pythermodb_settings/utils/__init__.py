4304bb055fb6a18e47508b12a827ad25e84002be
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-09-19 23:36:53 -0400

    add method :: create mixture id

--- pythermodb_settings/utils/__init__.py
+++ pythermodb_settings/utils/__init__.py
@@ -1,7 +1,12 @@
 # export
-from .component_utils import create_component_id, set_component_id
+from .component_utils import (
+    create_component_id,
+    set_component_id,
+    create_binary_mixture_id
+)
 
 __all__ = [
     "create_component_id",
-    "set_component_id"
+    "set_component_id",
+    "create_binary_mixture_id",
 ]
