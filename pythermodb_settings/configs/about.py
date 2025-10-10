33746e3879bf8f8374b992e1f578696c895ff29e
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-10-10 19:38:32 -0400

    upg pkg v0.1.9

--- pythermodb_settings/configs/about.py
+++ pythermodb_settings/configs/about.py
@@ -1,5 +1,5 @@
 # NOTE: app info
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 __description__ = """PyThermoDB Settings is a Python package that acts as an interface between pythermdb and other applications, providing robust, Pydantic-based data models and configuration structures for managing thermodynamic database (ThermoDB) settings."""
 __author__ = "Sina Gilassi"
 __author_email__ = "sina.gilassi@gmail.com"
