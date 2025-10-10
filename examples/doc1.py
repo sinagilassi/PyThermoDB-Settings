1483910d17e82c0661d1cd138b4e682fe7db20e2
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-10-10 14:14:24 -0400

    test passed

--- examples/doc1.py
+++ examples/doc1.py
@@ -1,6 +1,7 @@
 # import libs
 from pythermodb_settings import __version__
 from pythermodb_settings.models import Component
+from pythermodb_settings.utils import set_component_id
 from rich import print
 
 # version info
@@ -14,3 +15,47 @@ print(comp)
 # NOTE: create a component with new keyword argument
 comp2 = Component(name="Ethanol", formula="C2H6O", state="l", CAS="64-17-5")
 print(comp2)
+
+# SECTION: set component id
+# ! by name
+print(set_component_id(
+    component=comp,
+    component_key='Name'
+))
+# ! by formula
+print(set_component_id(
+    component=comp2,
+    component_key='Formula'
+))
+# ! by name-state
+print(set_component_id(
+    component=comp2,
+    component_key='Name-State'
+))
+# ! by formula-state
+print(set_component_id(
+    component=comp2,
+    component_key='Formula-State'
+))
+# ! by name-formula-state
+print(set_component_id(
+    component=comp2,
+    component_key='Name-Formula-State'
+))
+# ! by formula-name-state
+print(set_component_id(
+    component=comp2,
+    component_key='Formula-Name-State'
+))
+# ! upper
+print(set_component_id(
+    component=comp2,
+    component_key='Name',
+    case='upper'
+))
+# ! lower
+print(set_component_id(
+    component=comp2,
+    component_key='Name',
+    case='lower'
+))
