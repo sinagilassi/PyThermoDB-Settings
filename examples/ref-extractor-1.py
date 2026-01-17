e5d807a6edc161f779a75e3d835d084871709e50
sina.gilassi <sina.gilassi@gmail.com>
2026-01-16 23:42:50 -0500

    exp

--- examples/ref-extractor-1.py
+++ examples/ref-extractor-1.py
@@ -1,13 +1,34 @@
 from pathlib import Path
 from pythermodb_settings.models import Component
-from pythermodb_settings.references import extract_reference_components
+from pythermodb_settings.references import (
+    extract_reference_components,
+    check_reference_component_availability
+)
 
 
 # H2O
-water = Component(name="water", formula="H2O", state="g")
+water = Component(name="dihydrogen monoxide", formula="H2O", state="g")
 # CO2
 CO2 = Component(name="carbon dioxide", formula="CO2", state="g")
 
+# --------------------------------------------------------------
+# SECTION: Check component availability in reference file
+# --------------------------------------------------------------
+availability_result = check_reference_component_availability(
+    reference=Path("private/reference_content.yaml"),
+    component_keys=['H2O', 'CO2', 'N2b'],
+    components=[water, CO2],
+    component_key="Name",
+    separator_symbol="-",
+    case=None,
+    renumber=False
+)
+print(availability_result)
+
+
+# --------------------------------------------------------------
+# SECTION: Extract components from reference file
+# --------------------------------------------------------------
 result = extract_reference_components(
     reference_file=Path("private/reference_content.yaml"),
     components=[water, CO2],
