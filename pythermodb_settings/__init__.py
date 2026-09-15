fd86f6286f4c05f9c971727f1975ae262ff10551
sina.gilassi <sina.gilassi@gmail.com>
2026-09-14 23:44:22 -0400

    feat: update __init__.py to include quantity-related imports and exports

--- pythermodb_settings/__init__.py
+++ pythermodb_settings/__init__.py
@@ -6,9 +6,29 @@ from .configs.about import (
     __author_email__,
 )
 
+# quantity-related imports
+from pythermodb_settings.models.quantity import (
+    QuantityDefinition,
+    QuantityRef,
+    QuantityRegistryData,
+)
+from pythermodb_settings.references.quantities import (
+    QuantityRegistry,
+    load_quantity_registry,
+    registry,
+)
+from pythermodb_settings.utils.quantity_meta import quantity_meta
+
 __all__ = [
     "__version__",
     "__description__",
     "__author__",
     "__author_email__",
+    "QuantityDefinition",
+    "QuantityRef",
+    "QuantityRegistryData",
+    "QuantityRegistry",
+    "load_quantity_registry",
+    "registry",
+    "quantity_meta",
 ]
